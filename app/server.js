f52b5375bc23305740c90d40cb81325aff2b42a0
samuele illuminati <sammyill91@gmail.com>
2026-03-20 12:04:14 +0100

    exercise 3

--- app/server.js
+++ app/server.js
@@ -10,6 +10,8 @@ app.get("/test", (req, res) => {
   res.send("this is test route one,hello i'm node");
 });
 
-
+app.get("/es3",(req,res)=>{
+  res.send("All the exsercises have been completed");
+})
 
 app.listen(3000, () => console.log("Server running on port 3000"));
